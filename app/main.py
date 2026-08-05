fb49fa3f48e1036058961e7f6b10b4609903ad5d
samdav2 <dawodujohn1@gmail.com>
2026-08-05 15:53:41 +0100

    adding google docs live

--- app/main.py
+++ app/main.py
@@ -30,9 +30,9 @@ limiter = Limiter(key_func=get_remote_address)
 app = FastAPI(
     title=settings.PROJECT_NAME,
     lifespan=lifespan,
-    docs_url=None,
-    redoc_url=None,
-    openapi_url=None,
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
 )
 app.state.limiter = limiter
 app.add_exception_handler(429, _rate_limit_exceeded_handler)
